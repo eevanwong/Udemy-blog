13d07fb2d44098b5f5f305bf686bcc9407f028d2
YaybeNo <evanmatt.wong@gmail.com>
2021-03-03 14:55:53 -0500

    Added Mongoose functionality

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const POSTS = [];
 
 const app = express();
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true} );
+mongoose.set('useFindAndModify', false);
+
+const postSchema = {
+  title: String,
+  content: String,
+}
+
+const Post = mongoose.model('Post', postSchema)
 
 app.set("view engine", "ejs");
 
@@ -22,10 +30,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  POSTS.forEach((element) => {
-    console.log(element.title);
-  });
-  res.render("home", { content: homeStartingContent, posts: POSTS });
+  Post.find({}, (err,foundPosts) => {
+    if (!err) {
+      console.log(foundPosts);
+      res.render("home", { content: homeStartingContent, posts: foundPosts });
+    }
+  })
+
 });
 
 app.get("/about", (req, res) => {
@@ -40,28 +51,32 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  let pName = _.lowerCase(req.params.postName);
-  POSTS.forEach((element) => {
-    if (pName === _.lowerCase(element.title)) {
-      console.log("match");
-      res.render("post", {
-        postTitle: element.title,
-        postContent: element.content,
-      });
+app.get("/posts/:postID", (req, res) => {
+  const postID = req.params.postID;
+  
+  Post.findOne({_id: postID.toLowerCase() }, (err,foundPost) => {
+    if (!err) {
+      res.render("post", {postTitle: foundPost.title,
+      postContent: foundPost.content})
+    } else {
+      console.log(err);
+      console.log("err getting custom post id url")
     }
-  });
+  })
+
 });
 
 app.post("/compose", (req, res) => {
-  const POST = {
+  const POST =  new Post({
     title: req.body.title,
     content: req.body.content,
-  };
+  });
 
-  POSTS.push(POST);
-
-  res.redirect("/");
+  POST.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function () {
